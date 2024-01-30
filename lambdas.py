82c78690d447730ed6ad4070240ae83fa5f4b185
Ngumo-1 <morrisngumo7@gmail.com>
2024-01-30 21:19:07 +0300

    implemented some lambda functions ie checking if a number is even, calculating squares of numbers and calculating area of rectangles

---
+++ lambdas.py
@@ -0,0 +1,8 @@
+def num_square(x):
+  return lambda x: x**2
+
+area=lambda l, w: l*w
+#function to calculate area of rectangle using length and width
+
+even_number= lambda x: x%2==0
+#function for checking if a number is even
